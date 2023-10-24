829a1dc0d0615ee6b95dc551eea20df14c0627de
Ian Painter <95984061+its-all-waves@users.noreply.github.com>
2023-10-24 20:12:16 +0000

    add optional params to newScene

--- src/lib/TableObjects/Scene.js
+++ src/lib/TableObjects/Scene.js
@@ -14,9 +14,21 @@ import { nanoid, NANO_ID_LENGTH } from '../util/util'
  */
 
 /**
- * @returns {TrackListItem} */
-export function newTrackListItem(trackName, charName) {
-	const characterNames = [charName]
+ * @returns {TrackListItem}
+ * */
+export function newTrackListItem(trackName, charName = null) {
+	// TODO: fill in 'context:' in below comment...
+	// charName not given         (context: when ??? we're adding to existing scenes (not starting from 0)? )
+	if (charName === null) {
+		let characterNames = []
+		return {
+			trackName,
+			characterNames
+		}
+	}
+
+	// charName is given
+	let characterNames = [charName]
 	return {
 		trackName,
 		characterNames
@@ -25,14 +37,52 @@ export function newTrackListItem(trackName, charName) {
 
 /**
  * @param {string} name
- * @returns {Scene} */
-export function newScene(name) {
+ * @returns {Scene}
+ * */
+export function newScene(name, scenes = null, tracks = null) {
 	const id = nanoid(NANO_ID_LENGTH)
 	const trackList = []
+
+	/* my thinking:
+	check what tracks currently exist
+	add those tracks to this scene's trackList as trackListItems with empty
+	characterNames[] */
+
+	// if we are creating the first scene...
+	if (scenes === null) {
+		return {
+			id,
+			name,
+			trackList
+		}
+	}
+
+	/* TODO: i feel like this logic may want to go under newTrack()... this is
+	true if the following procedure would be called far less times if in
+	newTrack(), but all else still right. */
+
+	// we're adding to existing scenes...
+
+	// add new trackListItems to trackList
+	for (let trackName of currentTrackNames(tracks)) {
+		trackList.push(newTrackListItem(trackName))
+	}
 
 	return {
 		id,
 		name,
 		trackList
 	}
+}
+
+/** Helper for newScene ...feels like there's a better way...
+ * Returns a list of current track names by creating a new set
+ * @returns {Set<string>}
+ */
+function currentTrackNames(tracksArr) {
+	let trackNames = new Set()
+	for (let track of tracksArr) {
+		trackNames.add(track.name)
+	}
+	return trackNames
 }
