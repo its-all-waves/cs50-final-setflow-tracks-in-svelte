f18cfc4e513060539ff2294e5d1205f4ee0fee89
Ian Painter <95984061+its-all-waves@users.noreply.github.com>
2023-11-05 03:24:52 +0000

    add first complex UI test: selected dropZone deselects if duplicate character grabbed

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,112 @@
+//@ts-check
+/** @typedef {import('@playwright/test').Locator} Locator */
+
 import { expect, test } from '@playwright/test'
 
-test('index page has expected h1', async ({ page }) => {
-	await page.goto('/')
-	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible()
+import { display } from '../src/lib/util/util'
+
+test('tracks page has expected heading', async ({ page }) => {
+	await page.goto('/tracks')
+	await expect(page.getByRole('heading', { name: 'Tracks' })).toBeVisible()
+})
+
+test.describe('selected drop zone rules', () => {
+	const SCENE_A = '33-A'
+	const SCENE_B = '66-B'
+	const TRACK_A = 'Track_3'
+	const TRACK_B = 'Track_4'
+	const CHARACTER = 'Né-né'
+
+	/** @type {Locator} */ let commitButton
+	/** @type {Locator} */ let characterPool
+	/** @type {Locator} */ let character
+	/** @type {Locator} */ let dropZoneA, dropZoneB
+	/** @type {Locator[]} */ let allDropZones
+
+	// get all the elements for the this batch of tests
+	test.beforeEach(async ({ page }) => {
+		await page.goto('/tracks')
+		commitButton = page.locator('#btn-commit')
+		characterPool = page.locator('.character-pool')
+		character = characterPool.getByText(CHARACTER)
+		dropZoneA = page.locator(
+			`[data-drop-zone][data-scene-name='${SCENE_A}'][data-track-name='${TRACK_A}']`
+		)
+		dropZoneB = page.locator(
+			`[data-drop-zone][data-scene-name='${SCENE_B}'][data-track-name='${TRACK_B}']`
+		)
+		allDropZones = await page.locator(`[data-drop-zone]`).all()
+	})
+
+	// test.afterEach(async ({ page }) => {
+	// 	// TODO: do i need this?
+	// 	await page.close()
+	// })
+
+	// TEST 1
+	test(`can add "${CHARACTER}" to "${SCENE_A}" > "${TRACK_A}"`, async ({ page }) => {
+		// pick up a character and put it in dropZone
+		await character.click()
+		await dropZoneA.click()
+		await commitButton.click()
+
+		// check that dropZone contains CHARACTER
+		const characterInDropZone = dropZoneA.locator('.character')
+		const innerText = await characterInDropZone.innerText()
+		expect(innerText).toBe(CHARACTER)
+	})
+
+	/**  */
+	// test(`rejects duplicate characters in scene`, async ({ page }) => {
+	test(`a selected drop zone deselects when grabbing a dupl character from pool`, async ({
+		page
+	}) => {
+		const TRACK = TRACK_A
+		const dropZone = dropZoneA
+
+		// grab the character, select the dropZone
+		await character.click()
+		await dropZone.click()
+
+		// assert: dropZone is selected after click
+		// let classes = await dropZone.getAttribute('class')
+		// expect(classes?.includes('selected')).toBeTruthy()
+		expect(dropZone).toHaveClass(/selected/)
+
+		// drop the character
+		await commitButton.click()
+
+		// assert: dropZone deselects after commit
+		// classes = await dropZone.getAttribute('class')
+		// expect(classes?.includes('selected')).toBeFalsy()
+		expect(dropZone).not.toHaveClass(/selected/)
+
+		// assert: character landed in the dropZone
+		const characterInDropZone = dropZone.locator('.character')
+		const innerText = await characterInDropZone.innerText()
+		expect(innerText).toBe(CHARACTER)
+
+		// select all dropZones on a track
+		const trackHeader = page.locator(`[data-track-row] th`).getByText(display(TRACK))
+		await trackHeader.click()
+
+		// assert: only dropZones on TRACK are selected
+		let selectedDropZones = await page.locator(`[data-drop-zone].selected`).all()
+		for (let _dropZone of selectedDropZones) {
+			await expect(_dropZone).toHaveAttribute('data-track-name', TRACK)
+		}
+
+		// grab the same character from the pool
+		await character.click()
+
+		// assert: all on TRACK are selected except dropZone
+		// dropZone not selected, and...
+		await expect(dropZone).not.toHaveClass(/selected/)
+		// ...rest on track are selected
+		selectedDropZones = await page.locator(`[data-drop-zone].selected`).all()
+		for (let _dropZone of selectedDropZones) {
+			await expect(_dropZone).toHaveClass(/selected/)
+		}
+		/// END OF TEST
+	})
 })
