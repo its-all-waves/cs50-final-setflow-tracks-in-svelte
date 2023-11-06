9ce4bb0e9b25f6ee2bd3ea2d08ba1e2b6e3c0cf8
Ian Painter <95984061+its-all-waves@users.noreply.github.com>
2023-11-06 04:01:45 +0000

    make <Header> better: change dataset attributes; get rid of ugly svelte if statement; add css back in

--- src/routes/tracks/header.svelte
+++ src/routes/tracks/header.svelte
@@ -40,18 +40,43 @@
 	}
 </script>
 
-{#if type === 'scene'}
-	<th
-		data-scene-col={name}
-		on:pointerup={setSelectedHeader(type, name)}
-	>
-		{display(name)}
-	</th>
-{:else if type === 'track'}
-	<th on:pointerup={setSelectedHeader(type, name)}>
-		{display(name)}
-	</th>
-{/if}
+<th
+	data-scene-header={type === 'scene' ? name : undefined}
+	data-track-header={type === 'track' ? name : undefined}
+	on:pointerup={setSelectedHeader(type, name)}
+>
+	{display(name)}
+</th>
 
 <style>
+	th {
+		margin: 0;
+		padding: 0;
+		text-align: center;
+		font-weight: bold;
+	}
+
+	[data-scene-header] {
+		position: sticky; /* currently does nothing... */
+		top: -0.6rem;
+		height: 3rem;
+		background: rgb(17, 25, 31);
+		background: linear-gradient(180deg, rgba(17, 25, 31, 1) 75%, rgba(17, 25, 31, 0) 100%);
+	}
+
+	/* row headers - keep narrow */
+	[data-track-header] {
+		position: sticky;
+		left: 0;
+		width: 12ch;
+		min-width: 9ch;
+		background: rgb(17, 25, 31);
+		background: linear-gradient(90deg, rgba(17, 25, 31, 1) 75%, rgba(17, 25, 31, 0) 100%);
+	}
+
+	th:hover {
+		text-shadow: 0 2px 1px black, 0px 0px 6px black, 0 0 6px rgba(255, 255, 255, 0.75),
+			0 0 12px rgba(255, 242, 0, 0.75), 0 0 18px rgba(255, 255, 255, 0.75);
+		cursor: pointer;
+	}
 </style>
